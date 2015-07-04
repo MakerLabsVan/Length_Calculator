a7d5ef4140fe8cabec444fba325e59e342e064d2
Jeremy <zhesah@gmail.com>
2015-07-04 14:42:40 -0700

    Added svg path parser.

--- Length Calculator.js
+++ Length Calculator.js
@@ -1,7 +1,26 @@
 	var bezier = require('bezier-js');
 	var fs = require('fs');
-	var readableStream = fs.createReadStream('/Users/Jeremy/Desktop/drawing.svg');
-	readableStream.setEncoding('utf8');
+    var data = '';
+    var parser = require('svg-path-parser');
+
+    var readableStream = fs.createReadStream('drawing.svg');
+    readableStream.setEncoding('utf8');
+
+    readableStream.on('data', function(chunk) {
+        var start = chunk.indexOf("id=\"path") - 1;
+        while(chunk.charAt(start) != '=' ){
+            start--;
+        }
+        start += 2;
+        var end = chunk.indexOf("\"", start);
+        var temp = chunk.substring(start,end);
+        console.log(parser(temp));
+    });
+ 
+    readableStream.on('end', function() {
+        
+    });
+
 
     function getLineLength(x_values, y_values){
         var length = 0;
@@ -11,5 +30,7 @@
         return length;
     }
 
-    var curve = new bezier(100,200 , 300,400 , 400,500 , 600,500);
-    console.log(curve.length());
+    function getArcLength(x_current, y_current, x_values, y_values){
+        var curve = new bezier(x_current,y_current , x_values[0],y_values[0] , x_values[1],y_values[1] , x_values[2],y_values[2])
+        return curve.length();
+    }
