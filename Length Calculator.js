20cd28cf60ad9039afa1eaa65b5738386e1c886a
Jeremy <zhesah@gmail.com>
2015-07-06 12:12:32 -0700

    Calculates any single line drawn. Does not matter if it is a combination of bezier curves and lines. Absolute/relative coordinates issue fixed.

--- Length Calculator.js
+++ Length Calculator.js
@@ -3,23 +3,83 @@ var bezier = require('bezier-js');          //from https://github.com/Pomax/bezi
 var fs = require('fs');                     
 var parser = require('svg-path-parser');    //from https://github.com/hughsk/svg-path-parser
 
-//initiate Stream to read from test file
+//initiate Stream to read from test file. Assuming test file is located in project folder
 var readableStream = fs.createReadStream('test.svg');
 readableStream.setEncoding('utf8');
 
+var length = 0.0;
+
 //Reads svg file, identifies path, and parses path
 readableStream.on('data', function(chunk) {
-    var start = chunk.indexOf("id=\"path") - 1;
-    while(chunk.charAt(start) != '=' ){
-        start--;
-    }
-    start += 2;
+    var start = chunk.indexOf("\n     d=\"") + 9;
     var end = chunk.indexOf("\"", start);
     var pathString = chunk.substring(start,end);
-    console.log(parser(pathString)); //implement method to take data from arrays and use functions to calculate length
+    var arrayOfValues = parser(pathString);
+    var currentX = 0.0;
+    var currentY = 0.0;
+
+    for (var i = 0; i < arrayOfValues.length; i++) {
+        var temp = arrayOfValues[i];
+        if(temp.command === "moveto"){
+            if(temp.relative){
+                currentX += temp.x;
+                currentY += temp.y;
+            }
+            else{
+                currentX = temp.x;
+                currentY = temp.y;
+            }
+        }
+        else if(temp.command === "lineto"){
+            if(temp.relative){
+                length += getLineLength([currentX, currentX + temp.x], [currentY, currentY + temp.y])
+                currentX += temp.x;
+                currentY += temp.y;
+            }
+            else{
+                length += getLineLength([currentX, temp.x], [currentY, temp.y]);
+                currentX = temp.x;
+                currentY = temp.y;
+            }
+        }
+        else if(temp.command === "curveto"){
+            if(temp.relative){
+                length += getArcLength(currentX, currentY, [currentX + temp.x1, currentX + temp.x2, currentX + temp.x], [currentY + temp.y1, currentY + temp.y2, currentY + temp.y]);
+                currentX += temp.x;
+                currentY += temp.y;
+            }
+            else{
+                length += getArcLength(currentX, currentY, [temp.x1, temp.x2, temp.x], [temp.y1, temp.y2, temp.y]);
+                currentX = temp.x;
+                currentY = temp.y;
+            }
+        }
+        else if(temp.command === "horizontal lineto"){
+            if(temp.relative){
+                length += getLineLength([currentX, currentX + temp.x], [currentY, currentY])
+                currentX += temp.x;
+            }
+            else{
+                length += getLineLength([currentX, temp.x], [currentY, currentY]);
+                currentX = temp.x;
+            }
+        }
+        else if(temp.command === "vertical lineto"){
+            if(temp.relative){
+                length += getLineLength([currentX, currentX], [currentY, currentY + temp.y])
+                currentY += temp.y;
+            }
+            else{
+                length += getLineLength([currentX, currentX], [currentY, currentY + temp.y]);
+                currentY = temp.y;
+            }
+        }
+    };
+
 });
  
 readableStream.on('end', function() {
+    console.log(length);
 });
 
 //Calculates length of line given array of x,y coordinates
