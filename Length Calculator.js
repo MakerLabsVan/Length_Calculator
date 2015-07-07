9e4fd3cb0aacb9c75a3009de00024c98582da83a
Jeremy <zhesah@gmail.com>
2015-07-07 12:37:51 -0700

    Added Elliptical Arc Length Computation.

--- Length Calculator.js
+++ Length Calculator.js
@@ -2,9 +2,10 @@
 var bezier = require('bezier-js');          //from https://github.com/Pomax/bezierjs
 var fs = require('fs');                     
 var parser = require('svg-path-parser');    //from https://github.com/hughsk/svg-path-parser
+var SVGCurveLib = require('/Users/Jeremy/node_modules/svg-curve-lib/src/js//svg-curve-lib.js');  //from https://github.com/MadLittleMods/svg-curve-lib
 
 //initiate Stream to read from test file. Assuming test file is located in project folder
-var readableStream = fs.createReadStream('test2.svg', { highWaterMark: 100000 });
+var readableStream = fs.createReadStream('test.svg', { highWaterMark: 100000 });
 readableStream.setEncoding('utf8');
 var length = 0.0;
 
@@ -26,127 +27,141 @@ readableStream.on('data', function(chunk) {
 
         for (var i = 0; i < arrayOfValues.length; i++) {
             var temp = arrayOfValues[i];
-            if(temp.command === "moveto"){
-                if(temp.relative){
-                    currentX += temp.x;
-                    currentY += temp.y;
-                    closeX += temp.x;
-                    closeY += temp.y;
-                }
-                else{
-                    currentX = temp.x;
-                    currentY = temp.y;
-                    closeX = temp.x;
-                    closeY = temp.y;
-                }
-            }
-            else if(temp.command === "lineto"){
-                if(temp.relative){
-                    length += getLineLength([currentX, currentX + temp.x], [currentY, currentY + temp.y])
-                    currentX += temp.x;
-                    currentY += temp.y;
-                }
-                else{
-                    length += getLineLength([currentX, temp.x], [currentY, temp.y]);
-                    currentX = temp.x;
-                    currentY = temp.y;
-                }
-            }
-            else if(temp.command === "horizontal lineto"){
-                if(temp.relative){
-                    length += getLineLength([currentX, currentX + temp.x], [currentY, currentY])
-                    currentX += temp.x;
-                }
-                else{
-                    length += getLineLength([currentX, temp.x], [currentY, currentY]);
-                    currentX = temp.x;
-                }
-            }
-            else if(temp.command === "vertical lineto"){
-                if(temp.relative){
-                    length += getLineLength([currentX, currentX], [currentY, currentY + temp.y])
-                    currentY += temp.y;
-                }
-                else{
-                    length += getLineLength([currentX, currentX], [currentY, temp.y]);
-                    currentY = temp.y;
-                }
-            }
-            else if(temp.command === "closepath"){
-                length += getLineLength([currentX, closeX], [currentY, closeY]);
-                currentX = closeX;
-                currentY = closeY;
-            }
-            else if(temp.command === "curveto"){
-                if(temp.relative){
-                    length += getArcLength(currentX, currentY, [currentX + temp.x1, currentX + temp.x2, currentX + temp.x], [currentY + temp.y1, currentY + temp.y2, currentY + temp.y]);
-                    smoothX = currentX + temp.x2;
-                    smoothY = currentY + temp.y2;
-                    currentX += temp.x;
-                    currentY += temp.y;
-                }
-                else{
-                    length += getArcLength(currentX, currentY, [temp.x1, temp.x2, temp.x], [temp.y1, temp.y2, temp.y]);
-                    currentX = temp.x;
-                    currentY = temp.y;
-                    smoothX = temp.x2;
-                    smoothY = temp.y2;
+            switch(temp.command){
+                case "moveto":
+                    if(temp.relative){
+                        currentX += temp.x;
+                        currentY += temp.y;
+                        closeX += temp.x;
+                        closeY += temp.y;
+                    }
+                    else{
+                        currentX = temp.x;
+                        currentY = temp.y;
+                        closeX = temp.x;
+                        closeY = temp.y;
+                    }
+                    break;
 
-                }
-            }
-            else if(temp.command === "smooth curveto"){
-                if(temp.relative){
-                    length += getArcLength(currentX, currentY, [currentX * 2 - smoothX, currentX + temp.x2, currentX + temp.x], [currentY * 2 - smoothY, currentY + temp.y2, currentY + temp.y]);
-                    currentX += temp.x;
-                    currentY += temp.y;
-                }
-                else{
-                    length += getArcLength(currentX, currentY, [currentX * 2 - smoothX, temp.x2, temp.x], [currentY * 2 - smoothY, temp.y2, temp.y]);
-                    currentX = temp.x;
-                    currentY = temp.y;
+                case "lineto":
+                    if(temp.relative){
+                        length += getLineLength([currentX, currentX + temp.x], [currentY, currentY + temp.y])
+                        currentX += temp.x;
+                        currentY += temp.y;
+                    }
+                    else{
+                        length += getLineLength([currentX, temp.x], [currentY, temp.y]);
+                        currentX = temp.x;
+                        currentY = temp.y;
+                    }
+                    break;
 
-                }
-            }
-            else if(temp.command === "quadratic curveto"){
-                if(temp.relative){
-                    length += getArcLength(currentX, currentY, [currentX + temp.x1, currentX + temp.x], [currentY + temp.y1, currentY + temp.y]);
-                    smoothX = currentX + temp.x1;
-                    smoothY = currentY + temp.y1;
-                    currentX += temp.x;
-                    currentY += temp.y;
-                }
-                else{
-                    length += getArcLength(currentX, currentY, [temp.x1, temp.x], [temp.y1, temp.y]);
-                    currentX = temp.x;
-                    currentY = temp.y;
-                    smoothX = temp.x1;
-                    smoothY = temp.y1;
+                case "horizontal lineto":
+                    if(temp.relative){
+                        length += getLineLength([currentX, currentX + temp.x], [currentY, currentY])
+                        currentX += temp.x;
+                    }
+                    else{
+                        length += getLineLength([currentX, temp.x], [currentY, currentY]);
+                        currentX = temp.x;
+                    }
+                    break;
 
-                }
-            }
-            else if(temp.command === "smooth quadratic curveto"){
-                if(temp.relative){
-                    length += getArcLength(currentX, currentY, [currentX * 2 - smoothX, currentX + temp.x], [currentY * 2 - smoothY, currentY + temp.y]);
-                    currentX += temp.x;
-                    currentY += temp.y;
-                }
-                else{
-                    length += getArcLength(currentX, currentY, [currentX * 2 - smoothX, temp.x], [currentY * 2 - smoothY, temp.y]);
-                    currentX = temp.x;
-                    currentY = temp.y;
-                }
-            }
-            else if(temp.command === "elliptical arc"){
-                console.log("Need to implement ellipital arc");
+                case "vertical lineto":
+                    if(temp.relative){
+                        length += getLineLength([currentX, currentX], [currentY, currentY + temp.y])
+                        currentY += temp.y;
+                    }
+                    else{
+                        length += getLineLength([currentX, currentX], [currentY, temp.y]);
+                        currentY = temp.y;
+                    }
+                    break;
+
+                case "closepath":
+                    length += getLineLength([currentX, closeX], [currentY, closeY]);
+                    currentX = closeX;
+                    currentY = closeY;
+                    break;
+
+                case "curveto":
+                    if(temp.relative){
+                        length += getCurveLength(currentX, currentY, [currentX + temp.x1, currentX + temp.x2, currentX + temp.x], [currentY + temp.y1, currentY + temp.y2, currentY + temp.y]);
+                        smoothX = currentX + temp.x2;
+                        smoothY = currentY + temp.y2;
+                        currentX += temp.x;
+                        currentY += temp.y;
+                    }
+                    else{
+                        length += getCurveLength(currentX, currentY, [temp.x1, temp.x2, temp.x], [temp.y1, temp.y2, temp.y]);
+                        currentX = temp.x;
+                        currentY = temp.y;
+                        smoothX = temp.x2;
+                        smoothY = temp.y2;
+                    }
+                    break;
+
+                case "smooth curveto":
+                    if(temp.relative){
+                        length += getCurveLength(currentX, currentY, [currentX * 2 - smoothX, currentX + temp.x2, currentX + temp.x], [currentY * 2 - smoothY, currentY + temp.y2, currentY + temp.y]);
+                        currentX += temp.x;
+                        currentY += temp.y;
+                    }
+                    else{
+                        length += getCurveLength(currentX, currentY, [currentX * 2 - smoothX, temp.x2, temp.x], [currentY * 2 - smoothY, temp.y2, temp.y]);
+                        currentX = temp.x;
+                        currentY = temp.y;
+
+                    }
+                    break;
+
+                case "quadratic curveto":
+                    if(temp.relative){
+                        length += getCurveLength(currentX, currentY, [currentX + temp.x1, currentX + temp.x], [currentY + temp.y1, currentY + temp.y]);
+                        smoothX = currentX + temp.x1;
+                        smoothY = currentY + temp.y1;
+                        currentX += temp.x;
+                        currentY += temp.y;
+                    }
+                    else{
+                        length += getCurveLength(currentX, currentY, [temp.x1, temp.x], [temp.y1, temp.y]);
+                        currentX = temp.x;
+                        currentY = temp.y;
+                        smoothX = temp.x1;
+                        smoothY = temp.y1;
+
+                    }
+                    break;
+
+                case "smooth quadratic cuveto":
+                    if(temp.relative){
+                        length += getCurveLength(currentX, currentY, [currentX * 2 - smoothX, currentX + temp.x], [currentY * 2 - smoothY, currentY + temp.y]);
+                        currentX += temp.x;
+                        currentY += temp.y;
+                    }
+                    else{
+                        length += getCurveLength(currentX, currentY, [currentX * 2 - smoothX, temp.x], [currentY * 2 - smoothY, temp.y]);
+                        currentX = temp.x;
+                        currentY = temp.y;
+                    }
+                    break;
+
+                case "elliptical arc":
+                    var ellipticalArcArcLengthResult = SVGCurveLib.approximateArcLengthOfCurve(1000, function(t) {
+                        return SVGCurveLib.pointOnEllipticalArc({x: currentX , y: currentY}, temp.rx, temp.ry, temp.xAxisRotation, temp.largeArc, temp.sweep, {x: temp.x, y: temp.y}, t);
+                    });
+                    length += ellipticalArcArcLengthResult.arcLength;
+                    break;
+
+                default:
+                    console.log("Error. Unknown path command.");
             }
             
         };
 
         indexOfLastLine = end;
         totalLines--;
-
     }
-
 });
  
 readableStream.on('end', function() {
@@ -163,7 +178,7 @@ function getLineLength(x_values, y_values){
 }
 
 //Calculates length of cubic bezier curve given array of x,y coordinates
-function getArcLength(x_current, y_current, x_values, y_values){
+function getCurveLength(x_current, y_current, x_values, y_values){
     if(x_values.length === 3)
         var curve = new bezier(x_current,y_current , x_values[0],y_values[0] , x_values[1],y_values[1] , x_values[2],y_values[2]);
     else
