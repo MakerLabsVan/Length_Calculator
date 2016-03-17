cc9792da8c020641425c29d1bbd5facd271e2352
Jeremy <zhesah@gmail.com>
2016-03-16 19:25:40 -0700

    Worked on test.html

--- test/raster_test.js
+++ test/raster_test.js
@@ -1,6 +1,23 @@
 describe("Testing Rastering Calculations", function () {
-	it("actually tests", function (){
-		expect(1).to.equal(1);
+	it("draws the visualization", function (){
+		testDraw("./test/test_files/circles3.png");
+		expect($("#canvas")[0]).to.not.equal(null);
 	});
-	
+	it("returns correct verticalDistance for circles3.png", function (){
+		testDraw("./test/test_files/circles3.png");
+		expect($("#verticalDistance").text()).to.equal("Vertical distance: 364 pixels");
+		
+	});
+	it("returns correct rasterlength for circles3.png", function (){
+		testDraw("./test/test_files/circles3.png");
+		expect($("#rasterlength").text()).to.equal(3814.07);
+	});
+	it("returns correct time for circles3.png", function (){
+		testDraw("./test/test_files/circles3.png");
+		expect($("#time").text()).to.equal(8.63);
+	});
+	it("returns correct cost for circles3.png", function (){
+		testDraw("./test/test_files/circles3.png");
+		expect($("#cost").text()).to.equal(15);
+	});
 });
